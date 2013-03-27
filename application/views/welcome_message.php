a308a1339ec9309100f0544bc31d43d984da45f1
unknown <Usuario@Usuario-PC.(none)>
2013-03-27 19:23:21 -0400

    prueba

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -27,7 +27,7 @@
             <nav>
                 <ul class="menu">
                     <li>
-                        <a href="/">Dinamica de Sistemas</a>
+                        <a href="/">OSITOTOTOTO</a>
                     </li>
                     <li>
                         <a href="nosotros/">Nosotros</a>
